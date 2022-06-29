711fd6603da52b294fb3917ca9fa8924ebcf5200
dynamic1717 <paska-andrej@yandex.ru>
2022-06-29 18:48:36 +0300

    Admin panel layout

---
+++ src/componenets/AdminEditCollectionModal/AdminEditCollectionModal.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { MultiSelect } from "react-multi-select-component";
+import Button from "../UI/Button/Button";
+import "./adminEditCollectionModal.scss";
+
+const AdminEditCollectionModal = ({
+  collection,
+  onClose,
+}: {
+  collection: any;
+  onClose: (event: React.SyntheticEvent) => void;
+}) => {
+  const [giveawayChannels, setGiveawayChannels] = useState([]);
+  const [announceChannels, setAnnounceChannels] = useState([]);
+  const [giveawayChanSelected, setGiveawayChanSelected] = useState([]);
+  const [annChanSelected, setAnnChanSelected] = useState([]);
+
+  useEffect(() => {
+    // TODO: rewrite it
+    const prepareValuesForMultiselect = () => {
+      collection.giveaways_channels.selected.forEach(ch => {
+        const newOption = { label: ch, value: ch };
+        setGiveawayChanSelected(prev => [...prev, newOption]);
+      });
+      collection.announcements_channels.selected.forEach(ch => {
+        const newOption = { label: ch, value: ch };
+        setAnnChanSelected(prev => [...prev, newOption]);
+      });
+
+      collection.giveaways_channels.list.forEach(ch => {
+        const newOption = { label: ch, value: ch };
+        setGiveawayChannels(prev => [...prev, newOption]);
+      });
+      collection.announcements_channels.list.forEach(ch => {
+        const newOption = { label: ch, value: ch };
+        setAnnounceChannels(prev => [...prev, newOption]);
+      });
+    };
+
+    prepareValuesForMultiselect();
+  }, [collection]);
+
+  return (
+    <div className="editModal">
+      <h2>Edit</h2>
+      <p>{collection.name}</p>
+
+      <div className="editModal__wrapper">
+        <div className="editModal__selectWrapper">
+          <span style={{ color: "#00b6a0" }}>Giveaway channels</span>
+          <MultiSelect
+            options={giveawayChannels}
+            value={giveawayChanSelected}
+            onChange={setGiveawayChanSelected}
+            labelledBy="Select giveaways"
+            hasSelectAll={false}
+            className="editModal__multiselect"
+          />
+        </div>
+
+        <div className="editModal__selectWrapper">
+          <span style={{ color: "#219cfb" }}>Announcement channels</span>
+          <MultiSelect
+            options={announceChannels}
+            value={annChanSelected}
+            onChange={setAnnChanSelected}
+            labelledBy="Select announcements"
+            hasSelectAll={false}
+            className="editModal__multiselect"
+          />
+        </div>
+      </div>
+
+      <div className="editModal__btns">
+        <Button size="large" variant="primary" onClick={onClose}>
+          Cancel
+        </Button>
+        <Button size="large" variant="gradient">
+          Save
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default AdminEditCollectionModal;
---
+++ src/pages/Admin/Admin.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
+import { Link } from "react-router-dom";
+import { useModal } from "../../app/useModal";
+import AdminEditCollectionModal from "../../componenets/AdminEditCollectionModal/AdminEditCollectionModal";
+import Button from "../../componenets/UI/Button/Button";
+import TextInput from "../../componenets/UI/TextInput/TextInput";
+import "./admin.scss";
+
+const mockCollection = {
+  name: "Super collection number 1",
+  giveaways_channels: { selected: ["channel 1"], list: ["channel 1"] },
+  announcements_channels: { selected: ["channel 2"], list: ["channel 1", "channel 2"] },
+};
+
+const mockCollection2 = {
+  name: "Another collection number 2",
+  giveaways_channels: {
+    selected: ["channel 2"],
+    list: ["channel 1", "channel 2", "channel 3", "channel 4"],
+  },
+  announcements_channels: {
+    selected: ["channel 1", "channel 2"],
+    list: ["channel 1", "channel 2", "channel 3", "channel 4"],
+  },
+};
+
+const mockAll = [mockCollection, mockCollection2];
+
+function Admin() {
+  const { toggle: toggleEditModal, hookModal } = useModal();
+  const [collectionToEdit, setCollectionToEdit] = useState(null);
+
+  const handlePageClick = () => {};
+
+  const openEditModal = (event, collection) => {
+    setCollectionToEdit(collection);
+    toggleEditModal(event);
+  };
+
+  return (
+    <main className="container admin">
+      <Button variant="link">
+        <Link to="/">Return to client side</Link>
+      </Button>
+
+      <section className="admin__table">
+        <div className="admin__header">
+          <TextInput placeholder="Search collection" />
+          <label className="admin__selectWrapper">
+            <span>Include giveaways channels</span>
+            <select name="giveaways" id="">
+              <option value="all">All</option>
+              <option value="with">With allocation</option>
+              <option value="without">Without allocation</option>
+            </select>
+          </label>
+          <label className="admin__selectWrapper">
+            <span>Include announcements channels</span>
+            <select name="announcements" id="">
+              <option value="all">All</option>
+              <option value="with">With allocation</option>
+              <option value="without">Without allocation</option>
+            </select>
+          </label>
+        </div>
+        <div className="admin__cols">
+          <p>Collection name</p>
+          <p>Giveaway channels</p>
+          <p>Announcements channels</p>
+        </div>
+
+        <div className="admin__body">
+          {mockAll.map((col, idx) => (
+            <article key={idx} className="admin__item" onClick={e => openEditModal(e, col)}>
+              <p>{col.name}</p>
+              <p>{col.giveaways_channels.selected.join("; ")}</p>
+              <p>{col.announcements_channels.selected.join("; ")}</p>
+            </article>
+          ))}
+        </div>
+
+        <div className="admin__footer">
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel=">"
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={3}
+            pageCount={10}
+            previousLabel="<"
+            renderOnZeroPageCount={null}
+            onPageChange={handlePageClick}
+            className="pagination"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            activeClassName="active"
+            disabledClassName="disabled"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+          />
+        </div>
+      </section>
+
+      {hookModal(
+        <AdminEditCollectionModal collection={collectionToEdit} onClose={toggleEditModal} />
+      )}
+    </main>
+  );
+}
+
+export default Admin;
